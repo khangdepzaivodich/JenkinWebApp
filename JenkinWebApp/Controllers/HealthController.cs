703579e599b6201df893621c4fab76ac3e65c6a7
khangdepzaivodich <phantuankhanghcmue@gmail.com>
2026-03-25 15:00:01 +0700

    feat: health check

---
+++ JenkinWebApp/Controllers/HealthController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JenkinWebApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new
+            {
+                Status = "Healthy",
+                Time = DateTime.Now
+            });
+        }
+    }
+}
